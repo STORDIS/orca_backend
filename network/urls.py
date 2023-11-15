070e1aea03b72dbd674033e95db40d307c8b421c
kamalkrbh <kamal.kr.bh@gmail.com>
2023-11-15 08:04:30 +0100

    Using re_path instead of path as arguments are passed as query parameters in the url i.e. using "?" and can be directly retrieved from request object. If using "path" you need to define explicitely define path with parameter passed and rest function should also have that param.

--- network/urls.py
+++ network/urls.py
@@ -1,18 +1,18 @@
-from django.urls import path
+from django.urls import re_path
 
 from . import views
 from . import vlan, interface, port_chnl, mclag, bgp, port_group
 
 urlpatterns = [
-    path("discover", views.discover, name="discover"),
-    path("devices", views.device_list, name="device_list"),
-    path("devices", views.device_list, name="device"),
-    path("interfaces", interface.device_interfaces_list, name="device_interface_list"),
-    path("port_chnls", port_chnl.device_port_chnl_list, name="device_port_chnl"),
-    path("mclags", mclag.device_mclag_list, name="device_mclag_list"),
-    path("bgp", bgp.device_bgp_global, name="bgp_global"),
-    path("bgp_nbrs", bgp.bgp_nbr_config, name="bgp_nbr"),
-    path("port_groups", port_group.port_groups, name="port_groups"),
-    path("gateway_mac", mclag.mclag_gateway_mac, name="mclag_gateway_mac"),
-    path("vlan", vlan.vlan_config, name="vlan_config"),
+    re_path("discover", views.discover, name="discover"),
+    re_path("devices", views.device_list, name="device_list"),
+    re_path("devices", views.device_list, name="device"),
+    re_path("interfaces", interface.device_interfaces_list, name="device_interface_list"),
+    re_path("port_chnls", port_chnl.device_port_chnl_list, name="device_port_chnl"),
+    re_path("mclags", mclag.device_mclag_list, name="device_mclag_list"),
+    re_path("bgp", bgp.device_bgp_global, name="bgp_global"),
+    re_path("bgp_nbrs", bgp.bgp_nbr_config, name="bgp_nbr"),
+    re_path("port_groups", port_group.port_groups, name="port_groups"),
+    re_path("gateway_mac", mclag.mclag_gateway_mac, name="mclag_gateway_mac"),
+    re_path("vlan", vlan.vlan_config, name="vlan_config"),
 ]
