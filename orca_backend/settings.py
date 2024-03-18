0bd32bc6b72fda2d336eec3de1cafc64106a6a88
rahul <rahul.s@metricdust.com>
2024-03-18 16:31:16 +0530

    adding authentication to all apis

--- orca_backend/settings.py
+++ orca_backend/settings.py
@@ -139,4 +139,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication'
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
