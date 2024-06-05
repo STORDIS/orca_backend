ee11a111c6f5918764ed6aa5366a5cb38a46d952
vivek-metricdust <vivek.v@metricdust.com>
2024-06-05 17:18:22 +0530

    delete device functionality with test case and url added

---
+++ network/test/test_delete_db.py
@@ -0,0 +1,73 @@
+from rest_framework import status
+
+from network.test.test_common import TestORCA
+
+
+class TestDelete(TestORCA):
+    
+    def test_delete_device(self):
+        
+        device_ip = "10.10.229.58"
+        request_body = {
+            "mgt_ip": device_ip,
+        }
+                
+        ## get discover device
+        response=self.get_req("device")
+        self.assertTrue(response.status_code == status.HTTP_200_OK)
+        
+        ## get interfaces list
+        response=self.get_req("device_interface_list", {"mgt_ip": device_ip})
+        self.assertTrue(response.status_code == status.HTTP_200_OK or response.status_code == status.HTTP_204_NO_CONTENT)
+        
+        ## get vlan list
+        response=self.get_req(
+            "vlan_config", {"mgt_ip": device_ip}
+        )
+        self.assertTrue(response.status_code == status.HTTP_200_OK or response.status_code == status.HTTP_204_NO_CONTENT)
+        
+        ## get port channel list
+        response=self.get_req(
+            "device_port_chnl", {"mgt_ip": device_ip}
+        )
+        self.assertTrue(response.status_code == status.HTTP_200_OK or response.status_code == status.HTTP_204_NO_CONTENT)
+        
+        ## get port group list
+        response=self.get_req(
+            "port_groups", {"mgt_ip": device_ip}
+        )
+        self.assertTrue(response.status_code == status.HTTP_200_OK or response.status_code == status.HTTP_204_NO_CONTENT)
+            
+        # delete the ip and its related connection from db
+        response=self.del_req("del_db_for_ip", request_body)
+        self.assertTrue(response.status_code ==  status.HTTP_200_OK or response.status_code == status.HTTP_204_NO_CONTENT) 
+        
+        # get discover device
+        response=self.get_req("device")
+        self.assertTrue(response.status_code == status.HTTP_204_NO_CONTENT)
+
+        ## get interface list
+        response=self.get_req("device_interface_list", {"mgt_ip": device_ip})
+        self.assertTrue(response.status_code == status.HTTP_204_NO_CONTENT)
+        
+        ## get vlan list
+        response=self.get_req(
+            "vlan_config", {"mgt_ip": device_ip}
+        )
+        self.assertTrue(response.status_code == status.HTTP_204_NO_CONTENT)
+        
+        ## get  port channel list
+        response=self.get_req(
+            "device_port_chnl", {"mgt_ip": device_ip}
+        )
+        self.assertTrue(response.status_code == status.HTTP_204_NO_CONTENT)
+        
+        ## get port group list
+        response=self.get_req(
+            "port_groups", {"mgt_ip": device_ip}
+        )
+        self.assertTrue(response.status_code == status.HTTP_204_NO_CONTENT)
+        
+
+        
+
--- network/views.py
+++ network/views.py
@@ -39,6 +39,48 @@ def delete_db(request):
 
 @api_view(
     [
+        "DELETE",
+    ]
+)
+@log_request
+def delete_db_for_ip(request):
+    """
+    A function that deletes the database.
+
+    Parameters:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        Response: The HTTP response object with the result of the deletion operation.
+    """
+    
+    result = []
+
+    if request.method == "DELETE":
+        from orca_nw_lib.device_db import delete_device
+        
+        try:
+            req_data_list = (
+                request.data if isinstance(request.data, list) else [request.data]
+            )
+            for req_data in req_data_list:
+                device_ip = req_data.get("mgt_ip", "")
+                del_res = delete_device(device_ip)
+                
+                if del_res:
+                    add_msg_to_list(result, get_success_msg(request))
+                else:
+                    add_msg_to_list(result,get_failure_msg(Exception("Failed to Delete"),request))
+                
+        except Exception as e:
+            return Response(
+                {"result": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+        return Response({"result": result}, status=status.HTTP_200_OK)
+
+
+@api_view(
+    [
         "PUT",
     ]
 )
