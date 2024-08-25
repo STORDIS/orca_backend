3fd9ff77f65c1ec8c8c6a818b51a10256f51a7d5
kamalkrbh <kamal.kr.bh@gmail.com>
2024-08-25 11:49:42 +0200

    Also test speed with multiple ethernet test.

--- network/test/test_common.py
+++ network/test/test_common.py
@@ -253,6 +253,7 @@ class TestORCA(APITestCase):
         Raises:
             AssertionError: If the URL name cannot be reversed.
         """
+        print(f"Sending GET request to URL {url_name}, with payload: {req_json}")
         return self.client.get(
             reverse(url_name),
             req_json,
@@ -273,6 +274,7 @@ class TestORCA(APITestCase):
         Raises:
             None.
         """
+        print(f"Sending DEL request to URL {url_name}, with payload: {req_json}")
         return self.client.delete(
             reverse(url_name),
             req_json,
@@ -290,6 +292,7 @@ class TestORCA(APITestCase):
         Returns:
             The response from the request in JSON format.
         """
+        print(f"Sending PUT request to URL {url_name}, with payload: {req_json}")
         return self.client.put(
             reverse(url_name),
             req_json,
@@ -397,10 +400,8 @@ class TestORCA(APITestCase):
                 return response
             except AssertionError:
                 print(
-                    f"Assertion failed for request args: {req_args}, and assert args: {assert_args}"
+                    f"!!! Assertion failed !!! Retrying in {timeout} seconds."
                 )
-                print(f"Response: {response.content}")
-                print(f"Retrying in {timeout} seconds")
                 time.sleep(timeout)
                 if i == retries:
                     raise ## If even after retries, assertion still fails, raise the exception
