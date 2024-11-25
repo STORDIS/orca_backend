890ed6c797c841b6494587aa9c88cc97b01fc03c
rahul <rahul.s@metricdust.com>
2024-11-25 12:05:01 +0530

    ssh connection chnages

--- fileserver/urls.py
+++ fileserver/urls.py
@@ -4,7 +4,7 @@ from fileserver import views
 urlpatterns = [
     re_path(r"^download/(.+)/$", views.download_file, name="download_file"),
     path("ztp", views.host_ztp_files, name="host_ztp_files"),
-    path("dhcp/credentials", views.dhcp_credentials, name="dhcp_credentials"),
+    path("dhcp/credentials", views.dhcp_auth, name="dhcp_credentials"),
     path("dhcp/config", views.dhcp_config, name="dhcp_config"),
     path("dhcp/backups", views.dhcp_backup, name="dhcp_backups"),
     path("dhcp/list", views.get_dhcp_device, name="dhcp_list"),
