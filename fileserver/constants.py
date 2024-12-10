9b9e9e1b4cb9d6b63f6687acf8968416e9e633e9
kamalkrbh <kamal.kr.bh@gmail.com>
2024-12-10 10:32:14 +0100

    Path for lease file corrected.

--- fileserver/constants.py
+++ fileserver/constants.py
@@ -1,6 +1,6 @@
 dhcp_schedule_interval = 15 * 60  # set to 15 minutes in seconds
 dhcp_path = "/etc/dhcp/"
-dhcp_leases_path = "/var/lib/dhcpd/dhcpd.leases"
+dhcp_leases_path = "/var/lib/dhcp/dhcpd.leases"
 dhcp_backup_prefix = "dhcpd.conf.orca."
 ztp_path = "media/download/ztp"
 ssh_key_path = "~/.ssh/orca/"
